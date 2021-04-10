be10da27d50e9b98c8d4d34767ee6aed35d72fc2
A <awongpro7@gmail.com>
2021-04-10 03:55:20 +0000

    Created todo list OO function for Todo List assignment

--- index.js
+++ index.js
@@ -5,76 +5,41 @@
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-
-// Old code
-// function newTask(title) {
-//   taskTitles.push(title);
-//   taskDescriptions.push(description);
-//   taskComplete.push(false);
-// }
-
-// New code
 function newTask(title, description) {
   const task = {
     title: title,
     description: description,
     complete: false,
 
+    // Print the state of a task to the console in a nice readable way
     logState: function() {
       console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     },
 
+    // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
     markCompleted: function() {
       this.complete = true;
     }
+
+
   };
   return task;
 }
 
-// prints out the provide task's details
-// code below moved up into newTask function, now old code
-// function logTaskState(task) {
-//   console.log(`${task.title} has${tasks.complete ? " " : " not "}been completed`)
-// }
 
-// marks the provided task as compelted
-// code below moved up into newTask function, now old code
-// function completeTask(task) {
-//   task.compelte = true;
-// }
 
-// DRUVER CODE BELOW
+// DRIVER CODE BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat LItter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
-
+// for now, let's just make sure we see our tasks
 console.log(tasks);
-
-task1.title
-task1.markCompleted();
-
-// // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-// function completeTask(taskIndex) {
-//   taskComplete[taskIndex] = true;
-// }
-
-// // Print the state of a task to the console in a nice readable way
-// function logTaskState(taskIndex) {
-//   const title = taskTitles[taskIndex];
-//   const complete = taskComplete[taskIndex];
-//   console.log(`${title} has${complete ? " " : " not "}been completed`);
-// }
-
-// // DRIVER CODE BELOW
 
 // newTask("Clean Cat Litter"); // task 0
 // newTask("Do Laundry"); // task 1
 
-// logTaskState(0); // Clean Cat Litter has not been completed
-// completeTask(0);
-// logTaskState(0); // Clean Cat Litter has been completed
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
